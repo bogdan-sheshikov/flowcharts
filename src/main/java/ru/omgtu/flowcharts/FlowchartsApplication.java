bd51b21ed30c8656b18c85b3fe8b21c034b04618
bogdan <bsheshikov@mail.ru>
2022-04-27 20:14:15 +0600

    Init commit

---
+++ src/main/java/ru/omgtu/flowcharts/FlowchartsApplication.java
@@ -0,0 +1,13 @@
+package ru.omgtu.flowcharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowchartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowchartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/omgtu/flowcharts/FlowchartsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.omgtu.flowcharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowchartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
